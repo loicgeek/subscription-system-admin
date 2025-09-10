2cf68b228b8e455da088989586a807ce1acf295d
loicgeek <loic.ngou98@gmail.com>
2025-09-10 02:29:18 +0100

    update api

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 use NtechServices\SubscriptionSystemAdmin\Http\Controllers\Api\PlanController;
 use NtechServices\SubscriptionSystemAdmin\Http\Controllers\Api\FeatureController;
 
-Route::prefix('subscription')->group(function () {
-    // Public routes
-    Route::get('/plans', [PlanController::class, 'index']);
-    Route::get('/plans/{plan}', [PlanController::class, 'show']);
-    Route::get('/plans/{plan}/features', [PlanController::class, 'features']);
-
-    
-});
+Route::get('/plans', [PlanController::class, 'index']);
+Route::get('/plans/{plan}', [PlanController::class, 'show']);
+Route::get('/plans/{plan}/features', [PlanController::class, 'features']);
