afbbd3c3897850c0d61781bdf52dd106e0acec2a
loicgeek <loic.ngou98@gmail.com>
2025-09-11 22:10:56 +0100

    latest stable

--- config/subscription-system-admin.php
+++ config/subscription-system-admin.php
@@ -3,8 +3,8 @@
 return [
     'enable_api_routes' => true,
     'enable_web_routes' => false,
-    'api_prefix' => 'api/subscription',
-    'web_prefix' => 'subscription',
+    'api_prefix' => 'api/ntech-subscription',
+    'web_prefix' => 'ntech-subscription',
     'api_middleware' => ['api'],
     'web_middleware' => ['web', 'auth'],
 ];
